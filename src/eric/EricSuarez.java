538911b98a8c2442dac38205ce606d77e4f115e8
EricGG2020 <ericksm2002@gmail.com>
2024-05-10 16:01:50 +0200

    agregamos el hola soy eric

---
+++ src/eric/EricSuarez.java
@@ -0,0 +1,16 @@
+package eric;
+
+public class EricSuarez {
+	public void printName() {
+		System.out.println("Hola soy Eric");
+	}
+	
+	public static void main (String[]args) {
+		EricSuarez eric=new EricSuarez();
+		eric.printName();
+	}
+	
+	
+	
+
+}
